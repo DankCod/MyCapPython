d323bb63598bd8d6ada8d82020c70e4e7cfd23fc
DankCod <106993525+DankCod@users.noreply.github.com>
2022-06-09 21:02:16 +0530

    Added files for Project 1.

---
+++ areaCircle.py
@@ -0,0 +1,3 @@
+x=float(input("Enter radius of circle:"));
+areaCircle=3.141592653589793*(x**2);
+print("Area of circle is",areaCircle);
---
+++ extension.py
@@ -0,0 +1,3 @@
+inputFile= input("Enter file name: ");
+extension= inputFile.split(".");
+print("The extension of your file is",extension[-1],".");
