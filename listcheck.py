d147795e00078fdfe9ad2d519933d4e1ded4fa0e
DankCod <106993525+DankCod@users.noreply.github.com>
2022-06-14 15:13:21 +0530

    Prints +ve numbers in given list.

---
+++ listcheck.py
@@ -0,0 +1,10 @@
+list1=[12,-7,5,64,-14];
+list2=[12,14,-95,3];
+print("The numbers in list1 are:")
+for number in list1:
+    if number>0:
+        print(number);
+print("The numbers in list2 are:")
+for number in list2:
+    if number>0:
+        print(number);
